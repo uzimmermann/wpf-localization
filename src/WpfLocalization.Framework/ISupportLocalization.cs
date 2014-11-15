59187cb8bf5c83f6304a4fc0882988074363779e
uzimmermann <Uwe.Zimmermann@icam-systems.de>
2014-11-15 18:54:58 +0100

    add interface to provide features required to support localization

---
+++ src/WpfLocalization.Framework/ISupportLocalization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WpfLocalization.Framework
+{
+    /// <summary>
+    /// Provides the features required to support localization.
+    /// </summary>
+    public interface ISupportLocalization
+    {
+        /// <summary>
+        /// Gets the identifier of this instance of an implementation of <see cref="ISupportLocalization"/>.      
+        /// </summary>
+        Guid Id { get; }
+
+        /// <summary>
+        /// Does the localization of this instance of an implementation of <see cref="ISupportLocalization"/>.
+        /// </summary>
+        void Localize();
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the localization of this instance is suspended.
+        /// </summary>
+        bool LocalizationIsSuspended { get; set; }
+
+        /// <summary>
+        /// Suspends localization of this instance of an implementation of <see cref="ISupportLocalization"/>.
+        /// </summary>
+        void SuspendLocalization();
+
+        /// <summary>
+        /// Resumes localization of this instance of an implementation of <see cref="ISupportLocalization"/>.
+        /// </summary>
+        void ResumeLocalization();
+    }
+}

31bb389ec57926cc3478b24e05f5d0233c1636fa
uzimmermann <Uwe.Zimmermann@icam-systems.de>
2014-11-15 12:16:49 +0100

    add class lib project for wpf localization framework

---
+++ src/app/WpfLocalization.Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("WpfLocalization.Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("WpfLocalization.Framework")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]

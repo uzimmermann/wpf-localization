60065aae35e4ce0d0e135d773a4bb79811bcb87e
uzimmermann <Uwe.Zimmermann@icam-systems.de>
2014-11-17 18:58:51 +0100

    add test class for property support specs

---
+++ tests/WpfLocalization.Framework.Tests/PropertySupportSpecs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Machine.Specifications;
+
+namespace WpfLocalization.Framework.Tests
+{
+    public class PropertySupportSpecs
+    {
+        [Subject(typeof(PropertySupport))]
+        public class When_extracting_property_name_and_no_lambda_expression_is_given
+        {
+            static Exception Exception;
+
+            Because of = () => Exception = Catch.Exception(() => PropertySupport.ExtractPropertyName<string>(null));
+
+            It should_fail = () => Exception.ShouldNotBeNull();
+        }
+    }
+}

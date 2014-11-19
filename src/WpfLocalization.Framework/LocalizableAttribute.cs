72ae09e74b95d61db82d59eaaae59fe2af4bc996
uzimmermann <Uwe.Zimmermann@icam-systems.de>
2014-11-19 10:54:39 +0100

    add attribute to indicate that a property is localizable

---
+++ src/WpfLocalization.Framework/LocalizableAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WpfLocalization.Framework
+{
+    /// <summary>
+    /// Indicates that a property is localizable.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class LocalizableAttribute : Attribute
+    {
+    }
+}
